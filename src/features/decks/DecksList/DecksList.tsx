669481470e1d589aa5d1c022efaa55c4754a2aee
Kirill190611 <k.igorevitch88@yandex.ru>
2024-07-22 21:42:46 +0300

    Practice with get request

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,11 @@
 import s from './DecksList.module.css'
+import {decksAPI} from "../decks-api.ts";
+import {useEffect} from "react";
 
 export const DecksList = () => {
+  useEffect(() => {
+    decksAPI.fetchDecks()
+  }, [])
+
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,42 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+
+export const decksAPI = {
+  fetchDecks() {
+    return instance.get<FetchDecksResponse>('v2/decks')
+  }
+}
+
+//types
+
+export type FetchDecksResponse = {
+  items: Deck[]
+  pagination: PaginationType
+}
+
+export type Deck = {
+  isFavorite: boolean
+  author: AuthorType
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
+
+export type AuthorType = {
+  id: string
+  name: string
+}
+
+export type PaginationType = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
