717f45d2abd476a4c7e871202ed061ec96d38c09
Kirill190611 <k.igorevitch88@yandex.ru>
2024-07-30 15:41:04 +0300

    Practice with Thunk and custom-hook

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,23 +1,9 @@
 import s from './DecksList.module.css'
-import {decksAPI} from "../decks-api.ts";
-import {useEffect} from "react";
-import {useAppDispatch} from "../../../app/store.ts";
-import {setDecksAC} from "../decks-reducer.ts";
 import {DeckItem} from "./DeckItem/DeckItem.tsx";
-import {selectDecks} from "../decks-selectors.ts";
-import {useSelector} from "react-redux";
+import {useFetchDecks} from "./DeckItem/useFetchDecks.ts";
 
 export const DecksList = () => {
-
-    const dispatch = useAppDispatch();
-    const decks = useSelector(selectDecks)
-
-    useEffect(() => {
-        decksAPI.fetchDecks()
-            .then(res => {
-                dispatch(setDecksAC(res.data.items))
-            })
-    }, [])
+    const {decks} = useFetchDecks()
 
     return <ul className={s.list}>
         {
---
+++ src/features/decks/decks-thunks.ts
@@ -0,0 +1,12 @@
+import {Dispatch} from "redux";
+import {decksAPI} from "./decks-api.ts";
+import {setDecksAC} from "./decks-reducer.ts";
+
+export const fetchDecksTC = () => {
+    return (dispatch: Dispatch) => {
+        decksAPI.fetchDecks()
+            .then(res => {
+                dispatch(setDecksAC(res.data.items))
+            })
+    }
+}
